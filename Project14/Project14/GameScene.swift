ba6b39041a6133f74a761bd0f76091b0afec178d
Jeff Eng <jeff@jeffeng.me>
2016-08-03 16:35:43 -0700

    initial set up of game. deleted some code and imported some project files as needed.

---
+++ Project14/Project14/GameScene.swift
@@ -0,0 +1,24 @@
+//
+//  GameScene.swift
+//  Project14
+//
+//  Created by Jeffrey Eng on 8/3/16.
+//  Copyright (c) 2016 Jeffrey Eng. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+       /* Called when a touch begins */
+        
+    }
+   
+//    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+//    }
+}
