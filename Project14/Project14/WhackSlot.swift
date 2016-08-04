a7d51d7401e15a4395386bf207add05678671785
Jeff Eng <jeff@jeffeng.me>
2016-08-04 16:02:55 -0700

    added the show behavior for the character nodes to appear

--- Project14/Project14/WhackSlot.swift
+++ Project14/Project14/WhackSlot.swift
@@ -11,6 +11,9 @@ import SpriteKit
 
 class WhackSlot: SKNode {
     
+    var visible: Bool = false
+    var isHit: Bool = false
+    
     var charNode: SKSpriteNode!
     
     func configureAtPosition(pos: CGPoint) {
@@ -30,6 +33,22 @@ class WhackSlot: SKNode {
         cropNode.addChild(charNode)
         
         addChild(cropNode)
+    }
+    
+    func show(hideTime hideTime: Double) {
+        if visible { return }
+        
+        charNode.runAction(SKAction.moveByX(0, y: 80, duration: 0.05))
+        visible = true
+        isHit = false
+        
+        if RandomInt(min: 0, max: 2) == 0 {
+            charNode.texture = SKTexture(imageNamed: "penguinGood")
+            charNode.name = "charFriend"
+        } else {
+            charNode.texture = SKTexture(imageNamed: "penguinEvil")
+            charNode.name = "charEnemy"
+        }
     }
     
 }
